6783d7f48274074d55983503835244a1681eeded
Nuno Nogueira <nunofmn@gmail.com>
2017-01-09 15:08:30 +0000

    Add initial subscription email template.

--- app/mailers/stars_mailer.rb
+++ app/mailers/stars_mailer.rb
@@ -1,18 +1,7 @@
 class StarsMailer < ApplicationMailer
   def recent_stars_notification(user, starred_repos)
-    mg_client = Mailgun::Client.new(api_key)
-    mg_client.send_message(domain, mail_params(user, starred_repos)) 
-  end
-
-  private 
-
-  def mail_params(user, repos)
-    {
-      :from => "rui@#{domain}",
-      :to => user.email,
-      :subject => 'This week stars',
-      :text => "This week you starred the following repos: #{repos.join(', ')}"
-    }
+    @repos = starred_repos
+    mail to: user.email, subject: "Gitchhikers: Your Github starred repos digest"
   end
 end
 
--- lib/tasks/email_tasks.rake
+++ lib/tasks/email_tasks.rake
@@ -3,7 +3,7 @@ require 'date'
 desc 'send digest email'
 task send_recent_stars_email: :environment do
   User.all.each do |user|
-    found_repos = StarredRepos.new(user.nickname).starred_repos
-    StarsMailer.recent_stars_notification(user, found_repos).deliver
+    repos = StarredRepos.new(user.nickname).starred_repos
+    StarsMailer.recent_stars_notification(user, repos).deliver
   end
 end
