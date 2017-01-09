39da88fc216e7caa74f50db5744c1400c40f5b15
Rui Mangas Pereira <ruijosemangas@gmail.com>
2017-01-09 23:34:30 +0000

    reordered repos in email; added number of stars per repo

--- app/mailers/stars_mailer.rb
+++ app/mailers/stars_mailer.rb
@@ -3,7 +3,7 @@ class StarsMailer < ApplicationMailer
 
   def recent_stars_notification(user, starred_repos)
     @size = starred_repos.size
-    @repos = starred_repos.sort_by { |repo| repo.fetch(:star_count) }.first(NUM_REPO)
+    @repos = starred_repos.sort_by { |repo| -repo.fetch(:star_count) }.first(NUM_REPO)
 
     mail to: user.email, subject: "Gitchhikers: Your Github starred repos digest"
   end
