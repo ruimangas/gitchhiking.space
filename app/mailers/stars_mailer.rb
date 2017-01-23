6f48258ebc7676a3857e09a652ca6faf6f9caf68
Rui Mangas Pereira <ruijosemangas@gmail.com>
2017-01-23 17:12:44 +0000

    project banner is now shown in emails

--- app/mailers/stars_mailer.rb
+++ app/mailers/stars_mailer.rb
@@ -2,6 +2,8 @@ class StarsMailer < ApplicationMailer
   NUM_REPO = 4
 
   def recent_stars_notification(user, starred_repos)
+    attachments.inline['header_mail.png'] = File.read("#{Rails.root}/app/assets/images/header_mail.png")
+
     @size = starred_repos.size
     @repos = starred_repos.sort_by { |repo| -repo.fetch(:star_count) }.first(NUM_REPO)
 
